de37b467f9f08ef885db490835bd548d76f30d98
刘佳兴 <liujiaxingemail@gmail.com>
2022-05-10 21:54:28 +0800

    chore: go on

--- go.mod
+++ go.mod
@@ -6,35 +6,20 @@ require (
 	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220418222510-f25a4f6275ed
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/stretchr/testify v1.7.1
-	github.com/wailsapp/wails/v2 v2.0.0-beta.36
+	go.lsp.dev/jsonrpc2 v0.9.0
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/google/uuid v1.1.2 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
-	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
-	github.com/labstack/echo/v4 v4.7.2 // indirect
-	github.com/labstack/gommon v0.3.1 // indirect
-	github.com/leaanthony/go-ansi-parser v1.0.1 // indirect
-	github.com/leaanthony/go-common-file-dialog v1.0.3 // indirect
-	github.com/leaanthony/gosod v1.0.3 // indirect
 	github.com/leaanthony/slicer v1.5.0 // indirect
-	github.com/mattn/go-colorable v0.1.11 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/pkg/browser v0.0.0-20210706143420-7d21f8c997e2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/tkrajina/go-reflector v0.5.5 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	github.com/wailsapp/mimetype v1.4.1 // indirect
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	github.com/segmentio/encoding v0.2.7 // indirect
+	go.lsp.dev/pkg v0.0.0-20210125030640-b6310ac75a91 // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
-	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"embed"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
+	"net/http"
 	"os"
 	"path"
+	"strconv"
 
 	"github.com/slowrookie/redis-web-manager/api"
 	_ "github.com/slowrookie/redis-web-manager/api"
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/logger"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/mac"
-	"github.com/wailsapp/wails/v2/pkg/options/windows"
+	"go.lsp.dev/jsonrpc2"
+	"golang.org/x/net/websocket"
 )
 
 var (
@@ -41,54 +44,161 @@ func main() {
 	log.SetOutput(io.MultiWriter(os.Stdout, f))
 	log.Println(fmt.Sprintf("Root Path: %s", api.APP_ROOT))
 
-	app := NewApp()
-	var about = make(map[string]string)
-	about["version"] = version
-	about["commit"] = commit
-	about["date"] = date
-	about["builtBy"] = builtBy
-	about["environment"] = MODE
-	app.About = about
-
-	err = wails.Run(&options.App{
-		Title:             "Redis Web Manager",
-		Width:             1024,
-		Height:            800,
-		MinWidth:          720,
-		MinHeight:         570,
-		DisableResize:     false,
-		Fullscreen:        false,
-		Frameless:         false,
-		StartHidden:       false,
-		HideWindowOnClose: false,
-		RGBA:              &options.RGBA{R: 255, G: 255, B: 255, A: 255},
-		Assets:            assets,
-		LogLevel:          logger.DEBUG,
-		OnStartup:         app.startup,
-		OnDomReady:        app.domReady,
-		OnShutdown:        app.shutdown,
-		Bind: []interface{}{
-			app,
-		},
-		// Windows platform specific options
-		Windows: &windows.Options{
-			WebviewIsTransparent: true,
-			WindowIsTranslucent:  true,
-			DisableWindowIcon:    false,
-		},
-		Mac: &mac.Options{
-			TitleBar:             mac.TitleBarDefault(),
-			WebviewIsTransparent: true,
-			WindowIsTranslucent:  true,
-			About: &mac.AboutInfo{
-				Title:   "Redis Web Manager",
-				Message: "Redis Web Manager is a desktop application developed with React & Golang, used to manage Redis, and supports multi-platform operation.",
-				Icon:    icon,
-			},
-		},
-	})
-
+	// static files
+	buildFiles, err := fs.Sub(assets, "web/build")
 	if err != nil {
-		log.Fatal(err)
+		panic(nil)
+	}
+
+	// static files
+	http.Handle("/", http.FileServer(http.FS(buildFiles)))
+
+	// server
+	http.Handle("/ws", websocket.Handler(func(ws *websocket.Conn) {
+		ctx := ws.Request().Context()
+		ServerConn(ctx, ws, jsonrpc2.HandlerServer(func(ctx context.Context, reply jsonrpc2.Replier, req jsonrpc2.Request) error {
+			dec := json.NewDecoder(bytes.NewReader(req.Params()))
+			switch req.Method() {
+			case "About":
+				var about = make(map[string]string)
+				about["version"] = version
+				about["commit"] = commit
+				about["date"] = date
+				about["builtBy"] = builtBy
+				about["environment"] = MODE
+				return reply(ctx, about, nil)
+			// Config
+			case "Config":
+				conf, err := api.DefaultConfig.Get()
+				return reply(ctx, conf, err)
+			case "Config.Set":
+				var config api.Config
+				if err := dec.Decode(&config); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				err := config.Set()
+				return reply(ctx, nil, err)
+			case "Config.CheckPort":
+				var port int
+				if err := dec.Decode(&port); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				err := api.DefaultConfig.CheckPort(fmt.Sprintf("%d", port))
+				return reply(ctx, nil, err)
+			// Connections
+			case "Connections":
+				connections, err := api.Connections()
+				return reply(ctx, connections, err)
+			case "Connection.Test":
+				var connection api.Connection
+				if err := dec.Decode(&connection); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				err := connection.Test()
+				return reply(ctx, nil, err)
+			case "Connection.Edit":
+				var connection api.Connection
+				if err := dec.Decode(&connection); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				err := connection.New()
+				return reply(ctx, nil, err)
+			case "Connection.Delete":
+				var id string
+				if err := dec.Decode(&id); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				connection, err := api.FindConnectionByID(id)
+				if err != nil {
+					return reply(ctx, nil, err)
+				}
+				err = connection.Delete()
+				return reply(ctx, nil, err)
+			case "Connection.Open":
+				var id string
+				if err := dec.Decode(&id); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				connection, err := api.FindConnectionByID(id)
+				if err != nil {
+					return reply(ctx, nil, err)
+				}
+				err = connection.Open()
+				return reply(ctx, nil, err)
+			case "Connection.Disconnection":
+				var id string
+				if err := dec.Decode(&id); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				connection, err := api.FindConnectionByID(id)
+				if err != nil {
+					return reply(ctx, nil, err)
+				}
+				err = connection.Disconnection()
+				return reply(ctx, nil, err)
+			case "Connection.Copy":
+				connection := &api.Connection{}
+				if err := dec.Decode(&connection); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				err := connection.New()
+				return reply(ctx, nil, err)
+			case "Connection.Command":
+				var cmd api.Command
+				if err := dec.Decode(&cmd); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				connection, err := api.FindConnectionByID(cmd.ID)
+				if err != nil {
+					return reply(ctx, nil, err)
+				}
+				ret, err := connection.Command(cmd)
+				return reply(ctx, ret, err)
+			case "Convert.Length":
+				var convert api.Convert
+				if err := dec.Decode(&convert); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				return reply(ctx, strconv.Itoa(convert.Length()), nil)
+			case "Convert.ToHex":
+				var convert api.Convert
+				if err := dec.Decode(&convert); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				return reply(ctx, convert.ToHex(), nil)
+			case "Convert.ToJson":
+				var convert api.Convert
+				if err := dec.Decode(&convert); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				return reply(ctx, convert.ToJson(), nil)
+			case "Convert.ToBinary":
+				var convert api.Convert
+				if err := dec.Decode(&convert); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				return reply(ctx, convert.ToBinary(), nil)
+			case "Convert.Base64ToText":
+				var convert api.Convert
+				if err := dec.Decode(&convert); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				return reply(ctx, convert.Base64ToText(), nil)
+			case "Convert.Base64ToJson":
+				var convert api.Convert
+				if err := dec.Decode(&convert); err != nil {
+					return jsonrpc2.ErrInvalidRequest
+				}
+				return reply(ctx, convert.Base64ToJson(), nil)
+			default:
+				return jsonrpc2.MethodNotFoundHandler(ctx, reply, req)
+			}
+		}))
+	}))
+
+	// config
+	conf, err := api.DefaultConfig.Get()
+	if err != nil {
+		panic(nil)
 	}
 }
